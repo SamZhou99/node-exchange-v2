aad5b06a880bd29a95e6fa4bfa0cf27284a606a8
sam.z <45636987@qq.com>
2022-03-10 20:43:10 +0800

    update app

--- public/app/js/chunk-2d2263d3.4137e96d.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2263d3"],{e88b:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"market bg-dark t-font"},[t._v(" Market ")])},i=[],o=(e("c9fd"),{name:"Market",components:{},data:function(){return{loading:!1}},destroyed:function(){},methods:{init:function(){}},mounted:function(){this.init()}}),c=o,r=e("2877"),s=Object(r["a"])(c,a,i,!1,null,"a92f7fbc",null);n["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d2263d3.4137e96d.js.map
