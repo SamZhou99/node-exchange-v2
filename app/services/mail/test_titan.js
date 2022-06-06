7883504fe132f087489e451d4ae3303aff5680ad
sam.z <45636987@qq.com>
2022-06-06 23:44:34 +0800

    update

--- app/services/mail/test_titan.js
+++ app/services/mail/test_titan.js
@@ -1,7 +1,8 @@
 const titan = require('./titan.js')
 
 async function init() {
-    await titan.sendMail("xpflash@gmail.com", "测试下", "邮件内容" + Math.floor(Math.random() * 999999))
+    const r = Math.floor(100000 + Math.random() * 899999)
+    await titan.sendMail("xpflash@gmail.com", "测试", `Code : <span style="color:#ffee00;background-color:#000000;font-size:2rem;padding:1rem;font-family:Verdana;">${r} </span>`)
 }
 
 
